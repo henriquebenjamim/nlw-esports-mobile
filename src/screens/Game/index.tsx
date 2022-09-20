892abeca80fabbc42e345c615ad849e02adb4412
henriquebenjamim <henriiqqq@gmail.com>
2022-09-19 22:48:28 -0300

    Added content to card without ads

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Image, TouchableOpacity, View} from 'react-native';
+import { FlatList, Image, TouchableOpacity, View, Text} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -76,8 +76,13 @@ export function Game() {
         )}
         horizontal
         style={styles.containerList}
-        contentContainerStyle={styles.contentList}
+        contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
         showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyListText}>
+            Não há anúncios publicados no momento.
+          </Text>
+        )}
         />
       </SafeAreaView>
     </Background>
